3a5b31dd281c15c20c09240d5d409d9a8cd9a69f
root <root@ip-172-31-33-102.us-east-2.compute.internal>
2026-03-19 13:43:53 +0000

    fix ami id

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_ec2" {
-  ami           = " ami-0b0b78dcacbab728f"  # Amazon Linux (example)
+  ami           = "ami-0b0b78dcacbab728f"  # Amazon Linux (example)
   instance_type = "t3.micro"
 
   tags = {
